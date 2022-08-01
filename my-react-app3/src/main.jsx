8afb4945c8d1f0205a2dfa6a71b640d5468bc320
Marina Khadieva <utabond89@gmail.com>
2022-08-01 14:24:32 +0300

    GetData не доступна

--- my-react-app3/src/main.jsx
+++ my-react-app3/src/main.jsx
@@ -7,77 +7,81 @@ const serverUrl1 = 'https://api.genderize.io';
 const serverUrl2 = 'https://api.nationalize.io';
 
 
-function getData(url) {
-	return fetch(url)
-		.then(response => response.json())
-		.then(result => result);
-
-
-	class Form extends React.Component {
-
-		constructor(props) {
-			super(props);
-			this.state = { name: "Marina", gender: undefined, country: undefined };
-			// Эта привязка обязательна для работы `this` в колбэке.
-			this.handleSubmit = this.handleSubmit.bind(this);
-		}
-
-
-		handleSubmit(e) {
-			e.preventDefault();
-			const url1 = `${serverUrl1}?name=${this.state.name}`;
-			const url2 = `${serverUrl2}?name=${this.state.name}`;
-			result1 = getData(url1);
-			this.setState({ gender: result1.gender });
-			result2 = getData(url2);
-			this.setState({ country: result2.country[0].country_id });
-			let finalResult = `${this.state.name} is ${this.state.gender} from ${this.state.country} `;
-		}
+async function getData(url) {
+	const response = await fetch(url);
+	const result = await response.json();
+	return result;
+}
 
 
 
-		render() {
-			return <form className="form" onSubmit={this.handleSubmit}>
-				<p>
-					<input defaultValue={this.state.name} placeholder="Введите имя" />
-					<button>Отправить</button>
-				</p>
-			</form>
-		}
+class Form extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = { name: "Marina", gender: undefined, country: undefined };
+		// Эта привязка обязательна для работы `this` в колбэке.
+		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
-	class ResultDiv extends React.Component {
-		render() {
-			return <div className="task__result">{this.props.result}</div>
-		}
+
+	handleSubmit(e) {
+		e.preventDefault();
+		const url1 = `${serverUrl1}?name=${this.state.name}`;
+		const url2 = `${serverUrl2}?name=${this.state.name}`;
+		result1 = getData(url1);
+		this.setState({ gender: result1.gender });
+		result2 = getData(url2);
+		this.setState({ country: result2.country[0].country_id });
+		let finalResult = `${this.state.name} is ${this.state.gender} from ${this.state.country} `;
+		alert(finalResult);
 	}
 
-	class Title extends React.Component {
-		render() {
-			return <h1>Проверка страны и имени</h1>
-		}
+
+
+	render() {
+		return <form className="form" onSubmit={this.handleSubmit}>
+			<p>
+				<input defaultValue={this.state.name} placeholder="Введите имя" />
+				<button>Отправить</button>
+			</p>
+		</form>
+	}
+}
+
+class ResultDiv extends React.Component {
+	render() {
+		return <div className="task__result">{this.props.result}</div>
+	}
+}
+
+class Title extends React.Component {
+	render() {
+		return <h1>Проверка страны и имени</h1>
+	}
+}
+
+class Genderize extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { result: "Here will be result" };
 	}
 
-	class Genderize extends React.Component {
-		constructor(props) {
-			super(props);
-			this.state = { result: "Here will be result" };
-		}
 
-
-		render() {
-			return <div className="container">
-				<div className="task__body">
-					<Title />
-					<Form />
-					<ResultDiv result={this.state.result} />
-				</div>
+	render() {
+		return <div className="container">
+			<div className="task__body">
+				<Title />
+				<Form />
+				<ResultDiv result={this.state.result} />
 			</div>
-		}
+		</div>
 	}
+}
 
-	ReactDOM.createRoot(document.getElementById('root')).render(
-		<React.StrictMode>
-			<Genderize />
-		</React.StrictMode>
-	)
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<Genderize />
+	</React.StrictMode>
+)
+
