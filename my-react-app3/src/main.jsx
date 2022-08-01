a46b50febaadf91946c1cd6b560f0cfb814f9882
Marina Khadieva <utabond89@gmail.com>
2022-08-01 14:05:08 +0300

    3 components instead of 5

--- my-react-app3/src/main.jsx
+++ my-react-app3/src/main.jsx
@@ -12,77 +12,76 @@ function getData(url) {
 		.then(response => response.json())
 		.then(result => result)
 		.then(alert(result));
-}
 
-class Form extends React.Component {
 
-	constructor(props) {
-		super(props);
-		this.state = { name: undefined, gender: undefined, country: undefined };
-		// Эта привязка обязательна для работы `this` в колбэке.
-		this.handleSubmit = this.handleSubmit.bind(this);
+	class Form extends React.Component {
+
+		constructor(props) {
+			super(props);
+			this.state = { name: "Marina", gender: undefined, country: undefined };
+			// Эта привязка обязательна для работы `this` в колбэке.
+			this.handleSubmit = this.handleSubmit.bind(this);
+		}
+
+
+		handleSubmit(e) {
+			e.preventDefault();
+			const url1 = `${serverUrl1}?name=${this.state.name}`;
+			const url2 = `${serverUrl2}?name=${this.state.name}`;
+			result1 = getData(url1);
+			this.setState({ gender: result1.gender });
+			result2 = getData(url2);
+			this.setState({ country: result2.country[0].country_id });
+			let finalResult = `${this.state.name} is ${this.state.gender} from ${this.state.country} `;
+		}
+
+
+
+		render() {
+			return <form className="form" onSubmit={this.handleSubmit}>
+				<p>
+					<input defaultValue={this.state.name} placeholder="Введите имя" />
+					<button>Отправить</button>
+				</p>
+			</form>
+		}
 	}
 
-
-	handleSubmit(e) {
-		e.preventDefault();
-		this.setState({ name: "????" })
-		const url1 = `${serverUrl1}?name=${this.state.name}`;
-		const url2 = `${serverUrl2}?name=${this.state.name}`;
-		result1 = getData(url1);
-		this.setState({ gender: result.gender });
-		result2 = getData(url2);
-		this.setState({ country: result2.country[0].country_id });
-		let finalResult = `${this.state.name} is ${this.state.gender} from ${this.state.country} `;
+	class ResultDiv extends React.Component {
+		render() {
+			return <div className="task__result">{this.props.result}</div>
+		}
 	}
 
-
-
-	render() {
-		return <form className="form" onSubmit={this.handleSubmit}>
-			<p>
-				<input placeholder="Введите имя" />
-				<button>Отправить</button>
-			</p>
-		</form>
-	}
-}
-
-class ResultDiv extends React.Component {
-	render() {
-		return <div className="task__result">{this.props.result}</div>
-	}
-}
-
-class Title extends React.Component {
-	render() {
-		return <h1>Проверка страны и имени</h1>
-	}
-}
-
-class Genderize extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { result: "Here will be result" };
+	class Title extends React.Component {
+		render() {
+			return <h1>Проверка страны и имени</h1>
+		}
 	}
 
+	class Genderize extends React.Component {
+		constructor(props) {
+			super(props);
+			this.state = { result: "Here will be result" };
+		}
 
-	render() {
-		return <div className="container">
-			<div className="task__body">
-				<Title />
-				<Form />
-				<ResultDiv result={this.state.result} />
+
+		render() {
+			return <div className="container">
+				<div className="task__body">
+					<Title />
+					<Form />
+					<ResultDiv result={this.state.result} />
+				</div>
 			</div>
-		</div>
+		}
 	}
-}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<Genderize />
-	</React.StrictMode>
-)
+	ReactDOM.createRoot(document.getElementById('root')).render(
+		<React.StrictMode>
+			<Genderize />
+		</React.StrictMode>
+	)
 
 
 /* handleSubmit(e) {
